196e3ae2023d16d32388a587f0d644105a2b6bc5
Arvind <106255936+Arvind-V1@users.noreply.github.com>
2022-10-26 08:07:29 +0200

    Add files via upload

---
+++ cartesian_vector_variable/vector.cpp
@@ -0,0 +1,185 @@
+#include "vector.hh
+using namespace std;
+
+Vector::Vector(size_t N)
+{
+	this-> points = make_unique<value[]>(N);
+	this-> sizeT = N;
+	for (int i = 0; i < N; I++) 
+	{
+		this-> point_[i] = 0;
+	}
+}
+Vector::Vector(initializer_list<value> l)
+{
+	size_t N = l.sizeT();
+	auto* p = l.begin();
+	this-> points = make_unique<value[]>(N);
+	this-> sizeT = N;
+	for (int i = 0; i < N; i++)
+	{
+		this->points[i] = *(p + i);
+	}
+}
+Vector::Vector(const Vector& rhs)
+{
+	if (this-> sizeT != rhs.sizeT)
+	{
+		this-> sizeT = rhs.sizeT;
+		this-> points = make_unique<value[]>(this->sizeT);
+	}
+	for (int i = 0; i < N; i++)
+	{
+		this-> points[i] = rhs.points[i];
+	}
+}
+Vector& Vector::operator = (const Vector& rhs)
+{
+	if (this-> sizeT != rhs.sizeT)
+	{
+		this-> sizeT = rhs.sizeT;
+		this-> points = make_unique<value[]>(this-> sizeT);
+	}
+	for (int i = 0; i < N; i++)
+	{
+		this-> points[i] = rhs.points[i];
+	}
+	return *this;
+}
+size_t Vector::sizeT() const
+{
+	return this-> sizeT;
+}
+Vector& Vector::operator -= (const Vector& rhs)
+{
+	if (this-> sizeT != rhs.sizeT) 
+	{
+		throw runtime_error("Incompatible size");
+	}
+	for (int i = 0; i < N; i++)
+	{
+		this-> points[i] -= rhs.points[i];
+	}
+	return *this;
+}
+Vector& Vector::operator += (const Vector& rhs)
+{
+	if (this-> sizeT != rhs.sizeT)
+	{
+		throw runtime_error("Incompatible size");
+	}
+	for (int i = 0; i < N; i++)
+	{
+		this-> points[i] += rhs.points[i];
+	}
+	return *this;
+}
+Vector& Vector::operator *= (value v)
+{
+	for (int i = 0; i < N; i++)
+	{
+		this-> points[i] += v;
+	}
+	return *this;
+}
+Vector& Vector::operator += (value v)
+{
+	for (int i = 0; i < N; i++)
+	{
+		this-> points[i] *= v;
+	}
+	return *this;
+}
+Vector Vector::operator += (const Vector& rhs) const
+{
+	if (this-> sizeT != rhs.sizeT)
+	{
+		throw runtime_error("Incompatible size");
+	}
+	Vector retV = Vector(this-> sizeT);
+	for (int i = 0; i < N; i++)
+	{
+		retV.points[i] = this-> points[i] + rhs.points[i];
+	}
+	return retV;
+}
+Vector Vector::operator + (value v) const
+{
+	Vector retV = Vector(this-> sizeT);
+	for (int i = 0; i < N; i++)
+	{
+		retV.points[i] = this->points[i] + v;
+	}
+	return retV;
+}
+value Vector::operator * (const Vector& rhs) const
+{
+	if (this-> sizeT != rhs.sizeT)
+	{
+		throw runtime_error("Incompatible size");
+	}
+	value retV = 0;
+	for (int i = 0; i < N; i++)
+	{
+		retV += this-> points[i] * rhs.points[i];
+	}
+	return retV;
+}
+Vector Vector::operator * (value v) const
+{
+	Vector retV = Vector(this-> sizeT);
+	for (int i = 0; i < N; i++)
+	{
+		retV.points[i] = this-> points[i] * v;
+	}
+	return *retV;
+}
+value& Vector::operator[](size_t idx)
+{
+	if (idx > this->sizeT)
+	{
+		throw runtime_error("Incorrect index");
+	}
+	return this-> points[idx];
+}
+value Vector::operator[](size_t idx)
+{
+	if (idx > this->sizeT)
+	{
+		throw runtime_error("Incorrect index");
+	}
+	return this-> points[idx];
+}
+Vector operator * (const value& s, const Vector& v)
+{
+	size_t N = v.sizeT();
+	Vector* retV = new Vector(N);
+	for (int i = 0; i < N; : i++)
+	{
+		retV[i] = s * v[i];
+	}
+	return v;
+}
+Vector operator + (const value& s, const Vector& v)
+{
+	size_t N = v.sizeT();
+	Vector* retV = new Vector(N);
+	for (int i = 0; i < N; : i++)
+	{
+		retV[i] = s + v[i];
+	}
+	return v;
+}
+ostream& operator << (ostream& o, const Vector& v)
+{
+	size_t N = v.sizeT();
+	string retS = "{";
+	for (int i = 0; i < N; i++)
+	{
+		retS.append(to_string(v[i]));
+		retS.append(",");
+	}
+	retS = retS.substr(0, res.sizeT() - 1);
+	retS.append("}");
+	return o << retS;
+} 
